0519a4230922f12ffc3349fc12949de8661bc5f1
DavidPav3l <david_marian@outlook.com>
2023-10-08 00:06:07 +0300

    bla bla

--- src/components/SliderforDays.jsx
+++ src/components/SliderforDays.jsx
@@ -39,7 +39,7 @@ function SliderforDays({ content }) {
 		<swiper-container ref={swiperRef} navigation="true" init="false" pagination="true">
 			{content.map((item, index) => (
 				<swiper-slide key={index}>
-					<span className="absolute bottom-3 left-3 block text-xl font-bold  md:text-2xl lg:text-3xl">{`Day ${item.day}`}</span>
+					<span className="absolute bottom-3 left-3 block text-xl font-bold md:text-2xl lg:text-3xl">{`Day ${item.day}`}</span>
 					<div className="mt-[18vh] flex w-[80%] flex-col items-center justify-start gap-3 px-7 py-20 text-center sm:ml-[5%] sm:mt-[14vh] sm:items-start sm:justify-start lg:ml-[10%]">
 						<h2 className="max-w-2xl px-3 text-center font-mont text-3xl sm:text-start md:text-4xl lg:text-5xl">
 							{item.title}
